2c52c712d99d67428fd5debf2514a9ea1da11825
JRTell <john-robin@jrtell.se>
2018-07-26 21:15:46 +0200

    Adjusted site title

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ module.exports = {
   ** Headers of the page
   */
   head: {
-    title: 'jrtell',
+    title: 'John-Robin Tell',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
