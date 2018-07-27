c3bbf721c6faf46bbd44140f5d004d09c87c6238
JRTell <john-robin@jrtell.se>
2018-07-28 00:08:43 +0200

    portrait

--- pages/hire.vue
+++ pages/hire.vue
@@ -2,6 +2,7 @@
   <div>
     <section class="section">
       <div class="container has-text-centered">
+        <!--<img class="portrait" src="http://res.cloudinary.com/jrtell/image/upload/fl_progressive,q_auto,w_1100,h_1100,c_crop,g_face,r_max/w_200/v1532716014/jrtell/Snapchat-483615629.jpg">-->
         <h1 class="main-title is-size-1">Hire me</h1>
       </div>
     </section>
@@ -13,3 +14,9 @@
     name: 'hire'
   }
 </script>
+
+<style>
+  .portrait {
+    margin-top: 20px;
+  }
+</style>
