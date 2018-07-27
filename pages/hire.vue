250be8f59029b39ebe8b2924df222461d5df9f4c
JRTell <john-robin@jrtell.se>
2018-07-27 19:03:45 +0200

    Added Hire Me page

---
+++ pages/hire.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <section class="section">
+      <div class="container">
+        <h1>Hire me</h1>
+      </div>
+    </section>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'hire'
+  }
+</script>
